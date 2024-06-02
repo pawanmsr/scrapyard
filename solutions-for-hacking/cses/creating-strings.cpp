86c5737d28a2762adf6e17e9a6123a177fa7088d
Pawan Mishra <pawanmsr@outlook.com>
2024-06-02 21:22:02 +0530

    add cses 1622

---
+++ solutions-for-hacking/cses/creating-strings.cpp
@@ -0,0 +1,75 @@
+/* 
+ * Problem Statement:
+ * https://cses.fi/problemset/task/1622
+ */
+
+/*
+ * Compile on windows powershell:
+ * g++ --std=c++2a -DLOCAL .\template.cpp -o template.exe
+ * 
+ * Other cpp standards: c++11, c++14, c++20, etc.
+ * 
+ * Compile on unix based system terminal:
+ * g++ --std=c++2a -DLOCAL template.cpp -o template.out
+ */
+
+// Include All Standard Template Library //
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long int ll;
+
+// Macro //
+#define X first
+#define Y second
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+
+// Global //
+
+
+// Functions //
+
+
+// Main //
+int main(int argc, char const *argv[])
+{
+    // ios_base::sync_with_stdio(false);
+    // cin.tie(NULL);
+
+    // cout << fixed << setprecision(15);
+
+    #ifndef ONLINE_JUDGE
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    #else
+    // online submission
+    #endif
+
+    #ifdef LOCAL
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #else
+    // online submission
+    #endif
+    
+    /* code */
+    string s;
+    cin >> s;
+
+    sort(s.begin(), s.end());
+
+    vector<string> ans;
+    bool next = true;
+    while (next) {
+        ans.push_back(s);
+        next = next_permutation(s.begin(), s.end());
+    }
+
+    cout << ans.size() << '\n';
+    for (string ans_i : ans) {
+        cout << ans_i << '\n';
+    }
+
+    return 0;
+}
