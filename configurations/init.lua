49fb6d97d04191f3cee169eb30e10c57fb5d697a
Pawan Mishra <pawanmsr@outlook.com>
2024-10-14 19:19:12 +0530

    nvim plugins

---
+++ configurations/init.lua
@@ -0,0 +1,12 @@
+-- disable compatibility to old-time vi
+vim.opt.compatible = false
+
+require("plugins")
+
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {desc = 'Telescope find files'})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {desc = 'Telescope live grep'})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {desc = 'Telescope buffers'})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {desc = 'Telescope help tags'})
+
+require('nvim-treesitter.configs').setup{highlight={enable=true}}
