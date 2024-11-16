85aa887c20d3bf3d15d8b84a045485ea92348b27
Pawan Mishra <pawanmsr@outlook.com>
2024-11-16 23:32:07 +0530

    language client

--- configurations/plugins.lua
+++ configurations/plugins.lua
@@ -8,6 +8,9 @@ vim.call('plug#begin')
 -- On-demand loading: loaded when the specified command is executed
 Plug('preservim/nerdtree', {['on'] = 'NERDTreeToggle'})
 
+Plug('autozimu/LanguageClient-neovim',
+    {['branch']: 'next', ['do']: 'bash install.sh'})
+
 -- Searching planets from telescope
 Plug('nvim-lua/plenary.nvim')
 Plug('nvim-telescope/telescope.nvim', {['branch'] = '0.1.x'})
--- configurations/plugins.vim
+++ configurations/plugins.vim
@@ -8,4 +8,10 @@ call plug#begin()
 " On-demand loading: loaded when the specified command is executed
 Plug 'preservim/nerdtree', {'on': 'NERDTreeToggle'}
 
+" Rust
+Plug 'autozimu/LanguageClient-neovim', {
+    \ 'branch': 'next',
+    \ 'do': 'bash install.sh',
+    \ }
+
 call plug#end()
