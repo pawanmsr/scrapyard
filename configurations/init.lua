0c70e0ab02ed61afbdec7a1e0c74492287a413d1
Pawan Mishra <pawanmsr@outlook.com>
2024-11-26 14:57:45 +0530

    bifurcate further

--- configurations/init.lua
+++ configurations/init.lua
@@ -7,14 +7,11 @@ vim.cmd([[
     " .vimrc
 ]])
 
-require("rc")
+-- order sensitive
 require("plugins")
-require("lsp")
 
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {desc = 'Telescope find files'})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {desc = 'Telescope live grep'})
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {desc = 'Telescope buffers'})
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, {desc = 'Telescope help tags'})
+require("rc")
+require("lsp")
+require("telescope")
 
 require('nvim-treesitter.configs').setup{highlight={enable=true}}
