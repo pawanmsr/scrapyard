ee8464590318f754c8ead060b166ff495f6832f9
Pawan Mishra <pawanmsr@outlook.com>
2024-11-26 14:50:44 +0530

    editor run commands

---
+++ configurations/lua/rc.lua
@@ -0,0 +1,28 @@
+-- editor run commands
+
+vim.g.mapleader = ' '
+
+local set = vim.opt
+
+set.wrap = true
+set.title = true
+set.spell = true
+set.autoread = true
+set.smartindent = true
+
+set.tabstop = 4
+set.shiftwidth = 4
+
+set.encoding = "utf8"
+
+set.list = true
+set.smarttab = true
+set.expandtab = true
+set.smartcase = true
+set.hlsearch = true
+set.incsearch = true
+set.number = true
+set.wildmenu = true
+set.showmatch = true
+set.ruler = true
+set.cursorline = true
